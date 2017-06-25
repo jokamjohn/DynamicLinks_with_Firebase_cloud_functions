c9d3bbdb95b6146d282778bd424852bb965daed1
John Kagga <johnkagga@gmail.com>
2017-06-25 17:30:48 +0300

    Ready for deployment

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const urlBuilder = require('build-url');
+const request = require('request-promise');
 
 admin.initializeApp(functions.config().firebase);
 
@@ -8,6 +9,35 @@ exports.postDynamicLink = functions.database.ref('Posts/{postId}')
     .onWrite(event => {
         let post = event.data.val();
         const postId = event.params.postId;
+
+        if (post.addedDynamicLink) {
+            return;
+        }
+
+        post.addedDynamicLink = true;
+
+        const socialDescription = `Arvana Blog - ${post.title}`;
+        const socialImageUrl = "http://res.cloudinary.com/jokam/image/upload/v1498378886/ar_blog_qeqjzu.png";
+
+        const options = {
+            method: 'POST',
+            uri: `https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=${functions.config().applinks.key}`,
+            body: {
+                "longDynamicLink": makeDynamicLongLink(postId, socialDescription, socialImageUrl)
+            },
+            json: true
+        };
+
+        request(options)
+            .then(function (parsedBody) {
+                console.log(parsedBody);
+                return parsedBody.shortLink;
+            })
+            .then((shortLink) => {
+                post.shareUrl = shortLink;
+                console.log('short link: ' + shortLink);
+                return event.data.ref.set(post);
+            })
 
     });
 
